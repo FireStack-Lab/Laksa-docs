9212b4ca76e1ff2e6d736c0032845a3f0c9d54f6
neeboo <ghostcorn@gmail.com>
2018-09-18 01:30:48 +0800

    > update

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,64 @@
+module.exports = {
+  base: "/Laksa-docs/",
+  locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    "/": {
+      lang: "en-US", // 将会被设置为 <html> 的 lang 属性
+      title: "Laksa",
+      description: "Zilliqa BlockChain Javascript API"
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "Laksa",
+      description: "Zilliqa BlockChain Javascript API"
+    }
+  },
+  themeConfig: {
+    activeHeaderLinks: true,
+    locales: {
+      "/": {
+        selectText: "Languages",
+        label: "English",
+        editLinkText: "Edit this page on GitHub",
+        serviceWorker: {
+          updatePopup: {
+            message: "New content is available.",
+            buttonText: "Refresh"
+          }
+        },
+        algolia: {},
+        nav: [
+          { text: "Intro", link: "/" },
+          { text: "Guide", link: "/guide/" },
+          { text: "External", link: "https://google.com" }
+        ],
+        sidebar: "auto"
+      },
+      "/zh/": {
+        // 多语言下拉菜单的标题
+        selectText: "选择语言",
+        // 该语言在下拉菜单中的标签
+        label: "简体中文",
+        // 编辑链接文字
+        editLinkText: "在 GitHub 上编辑此页",
+        // Service Worker 的配置
+        serviceWorker: {
+          updatePopup: {
+            message: "发现新内容可用.",
+            buttonText: "刷新"
+          }
+        },
+        // 当前 locale 的 algolia docsearch 选项
+        algolia: {},
+        nav: [
+          { text: "介绍", link: "/zh/" },
+          { text: "指南", link: "/zh/guide/" },
+          { text: "Google", link: "https://google.com" }
+        ],
+        sidebar: "auto"
+      }
+    },
+    displayAllHeaders: true // 默认值：false
+  }
+};
---
+++ docs/README.md
@@ -0,0 +1,36 @@
+# Laksa
+
+![travis](https://travis-ci.com/FireStack-Lab/Laksa.svg?branch=master)
+[![npm version](https://img.shields.io/npm/v/laksa.svg?style=flat-square)](https://www.npmjs.org/package/laksa)
+
+## Introduction
+
+Laksa is a developer tools that interigate with Zilliqa-BlockChain.
+
+## Warning
+
+This project is under developement and updated frequently, including `test scripts` and `dependencies`.
+
+So it may change a lot before it's stable version released.
+
+`DO NOT` use it into your production envirorment.
+
+## Why `Laksa` but not `Zilliqa-JS`?
+
+- `Zilliqa-JS` is developed by Zilliqa Core Team. `Laksa` is developed by Zilliqa community.
+- `Zilliqa-JS` is a basic library that contains crypto algos, and basic utils.
+- `Laksa` is a framework that provides more features than `Zilliqa-JS`.
+- `Laska` use "mono-repo" that devides the repo to multiple sub packages. Developers may customize their project using different packages on their demands.
+- `Laksa` will grow as the community grows, not just `Javascript`, and more language support join the `Laksa` family.
+
+## How to Use?
+
+- [Getting Started](./guide/README.md)
+
+## API Reference
+
+! Coming Soon !
+
+## Examples
+
+! Coming Soon !
