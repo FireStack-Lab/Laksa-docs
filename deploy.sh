6ef919a7c5bc077b06b328528a215366f3680361
neeboo <ghostcorn@gmail.com>
2018-09-19 00:41:02 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
+# git init
 git add -A
 git commit -m 'deploy'
 
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:FireStack-Lab/Laksa-docs.git master:gh-pages
-
+# git push -f git@github.com:FireStack-Lab/Laksa-docs.git master:gh-pages
+# git push -f git@github.com:FireStack-Lab/Laksa-docs.git master
+git push
 cd -
